de08a39cc1ce9cba112294967c7c97797715f899
louis cailliot <louis.cailliot@thalesgroup.com>
2021-04-06 11:38:35 +0200

    #4 user environment with install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,19 @@
 
 CURRENT_PATH="$(realpath "$0")"
 CURRENT_DIR="$(dirname "${CURRENT_PATH}")"
-ln -s "${CURRENT_DIR}/easymount/easymount.py" /usr/local/bin/easymount
+ENV_FILE="${CURRENT_DIR}/.env"
+
+# install env
+echo ". Create user environment"
+cp "${ENV_FILE}.bak" "${ENV_FILE}"
+sed -i -e "s+{{EASYMOUNT_ROOT_DIR}}+${CURRENT_DIR}+g" "${ENV_FILE}"
+
+# install easymount
+easymount_cmd="/usr/local/bin/easymount"
+if [ ! -f "${easymount_cmd}" ]; then
+  echo ". Install easymount"
+  sudo ln -s "${CURRENT_DIR}/easymount/easymount.py" "${easymount_cmd}"
+fi
+
+echo ". Done"
+exit 0
